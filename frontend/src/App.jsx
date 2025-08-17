007fda93ec2cc0ae84e2bea359e3b6ed9396f418
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-18 01:39:16 +0530

    Refactor App component to implement routing with react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import EmployeeList from "./components/EmployeeList";
 import Footer from "./components/Footer";
+import InsertEmployee from "./components/InsertEmployee";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <EmployeeList />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<EmployeeList />} />
+          <Route path="/insert" element={<InsertEmployee />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
