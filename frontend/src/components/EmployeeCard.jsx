c1ad0dbfc3ace61323d4fdf6dce0f1f58ef70774
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-18 01:39:38 +0530

    Refactor EmployeeCard to implement delete functionality and update button actions

--- frontend/src/components/EmployeeCard.jsx
+++ frontend/src/components/EmployeeCard.jsx
@@ -5,12 +5,23 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
 
 const EmployeeCard = ({ employee }) => {
+  const onDeleteEmployee = (id) => {
+    axios
+      .delete(`http://localhost:4000/api/employees/${id}`)
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
-        sx={{ height: 140 }}
+        sx={{ height: 240 }}
         image="https://png.pngtree.com/png-clipart/20240310/original/pngtree-working-employee-3d-character-illustration-png-image_14552818.png"
         title="green iguana"
       />
@@ -27,8 +38,12 @@ const EmployeeCard = ({ employee }) => {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small" onClick={() => onDeleteEmployee(employee._id)}>
+          Delete
+        </Button>
+        <Button size="small" to={`/showdetails/${employee._id}`}>
+          Display
+        </Button>
       </CardActions>
     </Card>
   );
